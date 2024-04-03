71aa6578cef8350b74bede0a447d4a0a3f7b95a7
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2024-04-03 23:22:53 +0300

    .

--- generator.go
+++ generator.go
@@ -16,10 +16,20 @@ import (
     которая может представлять собой набор значений. Значение тега в свою очередь может представлять собой пару ключ=значение или просто ключ (required например)
 */
 
-type MethodLabelInfo struct {
+type Label struct {
 	URL    string
 	Auth   bool
 	Method string
+}
+
+type Field struct {
+	Name string
+	Type string
+}
+
+type Method struct {
+	Name       string
+	Parameters []Field
 }
 
 // CollectMethodsInfo собирает информацию о методах, для которых нужно сгенерировать код
@@ -33,12 +43,15 @@ func CollectMethodsInfo(filename string) {
 	for _, d := range root.Decls {
 		switch v := d.(type) {
 		case *ast.FuncDecl:
-			if v.Recv != nil {
+			if v.Recv != nil { // значит это метод
 				if v.Doc != nil {
 					label := v.Doc.Text()
-					if strings.Index(label, "{") != -1 { // определяем, является ли комментарий меткой для генерации кода
-						label = label[strings.Index(label, "{"):]
-						ParseMethodLabel(label)
+					if strings.Index(label, "{") != -1 { // проверяем, есть ли в комментарии метка для генерации кода
+						label = label[strings.Index(label, "{")+1 : strings.Index(label, "}")]
+						_ = ParseMethodLabel(label) // todo
+
+						methodInfo := ParseAPIMethod(v)
+						fmt.Println(*methodInfo)
 					}
 				}
 			}
@@ -46,8 +59,38 @@ func CollectMethodsInfo(filename string) {
 	}
 }
 
-func ParseMethodLabel(label string) *MethodLabelInfo {
-	pairs := strings.TrimRightstrings.TrimLeft(label, "{")
-	fmt.Print(pairs)
-	return nil
+func ParseAPIMethod(method *ast.FuncDecl) *Method {
+	inf := &Method{}
+	inf.Name = method.Name.Name
+	if params := method.Type.TypeParams; params != nil {
+		for _, el := range params.List {
+			f := Field{}
+			f.Name = el.Names[0].Name
+			if TypeInfo, ok := el.Type.(*ast.Ident); ok {
+				f.Type = TypeInfo.Name
+			}
+			inf.Parameters = append(inf.Parameters, f)
+		}
+	}
+	return inf
+}
+
+func ParseMethodLabel(label string) *Label {
+	pairs := strings.Split(strings.TrimRight(strings.TrimLeft(label, "{"), "}"), ", ")
+	mlInfo := &Label{}
+	if len(pairs) == 3 {
+		mlInfo.URL = strings.Trim(strings.Split(pairs[0], ": ")[1], `"`)
+		authVal := strings.Split(pairs[1], ": ")[1]
+		if authVal == "true" {
+			mlInfo.Auth = true
+		}
+		mlInfo.Method = strings.Trim(strings.Split(pairs[2], ": ")[1], `"'`)
+	} else if len(pairs) == 2 {
+		mlInfo.URL = strings.Trim(strings.Split(pairs[0], ": ")[1], `"`)
+		authVal := strings.Split(pairs[1], ": ")[1]
+		if authVal == "true" {
+			mlInfo.Auth = true
+		}
+	}
+	return mlInfo
 }
